663fcd705b107841b6d325ef7b548049ceaba06f
cristian <cchura.cpc@gmail.com>
2025-03-18 20:53:28 -0400

    Servicios Guards

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,8 @@ import Nosotros from '../views/web/Nosotros.vue';
 import Servicios from '../views/web/Servicios.vue';
 import Login from '../views/auth/Login.vue';
 import Blog from '../views/web/Blog.vue';
+import Perfil from '../views/admin/perfil/Perfil.vue';
+import Usuario from '../views/admin/user/Usuario.vue';
 
 const routes = [
     { path: "/", component: Inicio },
@@ -12,7 +14,21 @@ const routes = [
     { path: "/blog", component: Blog },
     {
         path: "/auth/login",
-        component: Login
+        component: Login,
+        name: 'Login',
+        meta: {redirectIfAuth: true}
+    },
+    {
+        path: '/admin/perfil',
+        component: Perfil,
+        name: 'MiPerfil',
+        meta: {requireAuth: true}
+    },
+    {
+        path: '/admin/usuario',
+        component: Usuario,
+        name: 'Usuario',
+        meta: {requireAuth: true}
     }
 ];
 
@@ -20,5 +36,22 @@ const router = createRouter({
     history: createWebHistory(),
     routes
 });
+
+// Guards
+router.beforeEach((to, from, next) => {
+    const token  = localStorage.getItem("access_token");
+
+    if(to.meta.requireAuth){
+        if(!token){
+            return next({name: "Login"})
+        }
+        return next();
+    }
+    if(to.meta.redirectIfAuth && token){
+        return next({name: 'MiPerfil'})
+    }
+    return next();
+
+})
 
 export default router;
